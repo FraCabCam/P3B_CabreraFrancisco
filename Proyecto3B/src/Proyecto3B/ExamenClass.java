6424b5246cfdc1042121526ff83fab0871e5b0d8
FraCabCam <fcabrera4084@ceuandalucia.es>
2023-11-30 16:29:40 +0100

    COMMIT A LA CLASE 1

---
+++ Proyecto3B/src/Proyecto3B/ExamenClass.java
@@ -0,0 +1,8 @@
+package Proyecto3B;
+
+public class ExamenClass {
+ public static void main(String[] args) {
+	System.out.println("Hola profesor, ponme un 10, soy Francisco Javier Cabrera Camino");
+}
+
+}
