9075552696d1b1eefc122529ecd3cdd84e3f567d
FraCabCam <151049354+FraCabCam@users.noreply.github.com>
2023-11-30 16:36:45 +0100

    Update ExamenClass.java
    MODIFICACION CLASE

--- Proyecto3B/src/Proyecto3B/ExamenClass.java
+++ Proyecto3B/src/Proyecto3B/ExamenClass.java
@@ -3,6 +3,8 @@ package Proyecto3B;
 public class ExamenClass {
  public static void main(String[] args) {
 	System.out.println("Hola profesor, ponme un 10, soy Francisco Javier Cabrera Camino");
+
+	 Syso.out.println("Estoy llegando al final");
 }
 
 }
